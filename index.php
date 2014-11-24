ec0514cb83b91a12c85c500513b9684b7d7664aa
Kevin Caraos <Kevincaraos@192-168-1-7.tpgi.com.au>
2014-11-24 21:27:18 +1100

    Started on Nav

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<title>Vienna Marie</title>
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/dist/css/bootstrap.css">
+
+</head>
+<body>
+	<div class="header">
+		<div class="brand">
+			<div class="logo">
+				<img src="" alt="index.php"> 
+			</div>
+			<div class="title">
+				<h1>Vienna Marie</h1>
+				<h2>The Little Lady with big dreams</h2>
+			</div>
+		</div>
+		<div class="nav-decor">
+		<div class="navbar">
+			<ul>
+				<li> <a href="about.php"> about </a></li>
+				<li> <a href="projects.php"> projects </a></li>
+				<li> <a href="resume.pdf"> resume </a></li>
+				<li> <a href="contact.php"> contact </a></li>
+			</ul>
+
+		</div>
+		</div>
+	</div>
+
+
+
+
+	<!-- jQuery JS -->
+	<script type="text/javascript" src="vendor/jquery/dist/jquery.js"></script>
+	<!-- Bootstrap JS -->
+	<script type="text/javascript" src="vendor/bootstrap/dist/js/bootstrap.js"></script>
+	<!-- Custom JS -->
+
+</body>
+</html>
